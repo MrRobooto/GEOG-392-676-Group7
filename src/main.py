2618eb491ad0d6f1fec9d5d7466ff205094dc5d7
Grayson <gmay4273@gmail.com>
2024-10-11 16:30:14 -0500

    Added Raster and Shapefile Masking

--- src/main.py
+++ src/main.py
@@ -1,12 +1,27 @@
-import rasterio
 from rasterio.plot import show
+#from gpd import *
+import rasterio
+import geopandas as gpd
+import pandas as pd
+from rasterio.mask import mask
 
-'''
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-INPUT_DB_PATH = BASE_DIR + "\data"
-CSV_PATH = BASE_DIR + "\data"
-OUTPUT_DB_PATH = BASE_DIR + "\scr\output"'''
 
-# Open and display the raster file
-with rasterio.open(r"C:\Users\gmay4\Downloads\polygonclip_2005749709.tar\polygonclip_2005749709\CACHE\VEGSCAPE\NDVI_2023\NDVI_50_20231204_20231217_clip_20241009160513_935735382.tif") as src:
-    show(src)
+def main():
+    path = r"C:\Users\Gray\OneDrive - Texas A&M University\Fall 2024\GEOG 392 500\Project\NDVI_2023\NDVI_45_20231030_20231112_clip_20241009160501_2125730837.tif"
+    cities_gdf = gpd.read_file(r"C:\Users\Gray\Downloads\tl_2017_48_place\tl_2017_48_place.shp")
+    
+        
+    
+    with rasterio.open(path) as src:
+        raster_crs = src.crs
+        
+        if cities_gdf.crs != raster_crs:
+            cities_gdf = cities_gdf.to_crs(src.crs)
+            
+        out_image, out_transform = mask(src, cities_gdf.geometry)
+        show(out_image)
+
+    
+
+if __name__ == "__main__":
+    main()
